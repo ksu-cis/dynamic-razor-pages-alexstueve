4c2bb00e431c0c9577ca2a7e6956b4fabf77a350
Alex Stueve <amstueve@ksu.edu>
2020-05-05 10:42:01 -0500

    5/5 - LINQ Queries

--- Movies/Pages/Index.cshtml.cs
+++ Movies/Pages/Index.cshtml.cs
@@ -12,30 +12,30 @@ namespace Movies.Pages
 
         public IEnumerable<Movie> Movies { get; protected set; }
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] MPAARatings { get; set; }
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public string[] Genres { get; set; }
 
-        [BindProperty]
+        [BindProperty (SupportsGet = true)]
         public string SearchTerms { get; set; } = "";
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMin { get; set; }
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? IMDBMax { get; set; }
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? RottenMax { get; set; }
 
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public double? RottenMin { get; set; }
 
         public void OnGet(double? imdbmin, double? imdbmax, double? rottenmin, double? rottenmax)
         {
-            this.IMDBMax = imdbmax;
+            /*this.IMDBMax = imdbmax;
             this.IMDBMin = imdbmin;
             this.RottenMin = rottenmin;
             this.RottenMax = rottenmax;
@@ -46,7 +46,68 @@ namespace Movies.Pages
             Movies = MovieDatabase.FilterByMPAARating(Movies, MPAARatings);
             Movies = MovieDatabase.FilterByGenre(Movies, Genres);
             Movies = MovieDatabase.FilterByIMDBRating(Movies, IMDBMin, IMDBMax);
-            Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RottenMin, RottenMax);
+            Movies = MovieDatabase.FilterByRottenTomatoesRating(Movies, RottenMin, RottenMax);*/
+
+            Movies = MovieDatabase.All;
+            if (SearchTerms != null) {
+                Movies = Movies.Where(movie => movie.Title != null && movie.Title.Contains(SearchTerms, StringComparison.CurrentCultureIgnoreCase));      
+            }
+
+            if (MPAARatings != null && MPAARatings.Length != 0)
+            {
+                Movies = Movies.Where(movie => movie.MPAARating != null && 
+                    MPAARatings.Contains(movie.MPAARating));
+            }
+
+            if (Genres != null && Genres.Count() != 0)
+            {
+                Movies = Movies.Where(movie => movie.MajorGenre != null &&
+                    Genres.Contains(movie.MajorGenre));
+            }
+
+            if (IMDBMin != null)
+            {
+                if (IMDBMax != null)
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating != null &&
+                        (movie.IMDBRating >= IMDBMin && movie.IMDBRating <= IMDBMax));
+                }
+                else
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating != null &&
+                        movie.IMDBRating >= IMDBMin);
+                }
+            }
+            else
+            {
+                if (IMDBMax != null)
+                {
+                    Movies = Movies.Where(movie => movie.IMDBRating != null &&
+                        movie.IMDBRating <= IMDBMax);
+                }
+            }
+
+            if (RottenMin != null)
+            {
+                if (RottenMax != null)
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating != null &&
+                        (movie.RottenTomatoesRating >= RottenMin && movie.RottenTomatoesRating <= RottenMax));
+                }
+                else
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating != null &&
+                        movie.RottenTomatoesRating >= RottenMin);
+                }
+            }
+            else
+            {
+                if (RottenMax != null)
+                {
+                    Movies = Movies.Where(movie => movie.RottenTomatoesRating != null &&
+                        movie.RottenTomatoesRating <= RottenMax);
+                }
+            }
         }
     }
 }
